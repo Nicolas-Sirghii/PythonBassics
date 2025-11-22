b2fb771c64e7de263ed35d43119298b3bd612546
Nicolas Sirghii <nicolas.mailbox100@gmail.com>
2025-11-22 15:22:11 +0200

    fist day

--- 22.11.26.py
+++ 22.11.26.py
@@ -1,3 +1,94 @@
-name = input()
-print("Hallo!")
-print(name)
+
+
+
+#name = input("insert your name ")
+#print("Hallo ", name , "I am the computer!")
+
+
+#a = input("insert the first word ")
+#b = input("insert the second name ")
+#c = a + " middle " + b
+#
+#print(c)
+
+
+"""
+
+pet = "cat"
+client = 'Петя'
+print(client)
+print(client+ " and " +pet)
+print(pet)
+
+
+
+
+r = 'Red'
+
+g = 'Green'
+
+b = 'Blue'
+
+print(r, b, g, r + g + b, b, g + b)
+
+
+
+
+
+first_animal = "hare"
+second_animal = "turtle"
+
+print(first_animal + " sleeps " + second_animal + " goes!")
+
+
+
+
+
+first_name = input('Введите имя пользователя: ')
+
+greeting = 'Утро доброе'
+
+print(greeting, first_name)
+
+intro = "К сожалению, у Вас нет доступа к системе."
+
+info = "Пожалуйста, обратитесь к системному администратору."
+
+print(intro, info)
+
+
+
+
+departure = input("Enter the departure city please. ")
+destination = input("Enter the destination city please. ")
+
+print(departure + " - " + destination)
+
+
+
+a = input('Введите первое слово: ')
+b = input('Введите второе слово: ')
+print(a, b)
+
+c = a
+a = b
+b = c
+
+
+print(a, b)
+
+
+
+
+
+
+
+a = input('Введите первое слово: ')
+b = input('Введите второе слово: ')
+print(a, b)
+
+a,b = b,a
+
+print(a, b)
+
+"""
